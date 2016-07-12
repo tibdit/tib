82533d049b0e2e7f8615c0d3d0fb8444e35ef8a6
Nadil <nadil.bourkadi@tibdit.com>
2016-07-12 14:58:15 +0100

    progress towards function tib callback

--- js/tib-callback.js
+++ js/tib-callback.js
@@ -1,3 +1,23 @@
 TibCallback = function(){
-    console.log('hi');
+    this.token = this.processToken();
+    console.log(this.token);
+};
+
+TibCallback.prototype.processToken = function(){
+    var token = this.extractUrlToken(window.location.href);
+    return token;
+};
+
+TibCallback.prototype.extractUrlToken = function(url){
+    // for clarity, steps are individually broken down, reusing a single variable
+
+    var token= new URI(url);
+
+    token= token.query(true); // retreive the querystring parameters into js object
+    token= token.tibtok; // pull out the value of the tibtok= querystring parameter
+    token= URI.decode(token); // convert any percent-encoded characters
+    token= atob(token); // base64 decode the token
+    token= JSON.parse(token); // convert the serialised json token string into js object
+
+    return token;
 };
