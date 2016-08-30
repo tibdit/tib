533eead5c4b0ecf5269907264cbef3d45f2afa7f
Justin Maxwell <justin@tibdit.com>
2016-08-30 22:58:41 +0100

    move sub-fns into closure - not sure will work

--- js/3.0.0/tib-3.0.0-button.js
+++ js/3.0.0/tib-3.0.0-button.js
@@ -12,10 +12,16 @@
 var TIBIT= (function(tibit){
 
     // Create our buttons object which will contain our buttons sub-namespace
+
+
     var buttons= {};
     tibit.tibButtons= [];
 
     var initButtons= function() {
+
+        // tibButtonDefaults={}  ??
+
+        // button defaults {} should be as argument to initButtons, and will available via closure.
 
         // instantiates and attaches a TibButton object to all DOM elements with the 'bd-tib-btn' class
         // settings are defaulted to matching items in the siteParams object, and data-bd-* attributes in the DOM element
@@ -34,104 +40,106 @@ var TIBIT= (function(tibit){
 
         // constructor for TibButton class, invoked by initButtons, argument is DOM element (the button)
 
+        this.domElement= e;
+        e.tibButton= tibButton= this;
+
         this.params = {};
 
         this.tibbed= false;
         this.testnet= false;
 
-        tibInitiator= new tibit.Initiator( e);
+        tibInitiator= e.tibInitiator= this.tibInitiator= new tibit.Initiator( e);
 
         tibit.copyParams( buttons.params, this.params);
         tibit.loadElementParams( this.params, e);   // Only needed in style()?
         tibit.loadElementParams( tibInitiator.params, e);
-
 
         // TODO: add TibInitiator this.storageKey property
         e.classList.add( tibIinitator.storageKey );  
 
         e.addEventListener("click", tibInitiator.dispatch.bind( tibInitiator));
         
-        window.addEventListener( 'tibstate', storageUpdate.bind(e));   // intra-window update trigger 
-        window.addEventListener( 'storage', storageUpdate.bind(e));   // inter-window update trigger
+        window.addEventListener( 'tibstate', storageUpdate.bind(this));   // intra-window update trigger 
+        window.addEventListener( 'storage', storageUpdate.bind(this));   // inter-window update trigger
 
-        setCounterElement( e);
+        setCounterElement( );
 
         if ( e.classList.contains('bd-dynamic'))   this.style();   // load and format a dynamic button
 
-        if ( tibInitiator.isTestnet() )   setTestnet(e);   // set 'demo mode' class on button
+        if ( tibInitiator.isTestnet() )   setTestnet();   // set 'demo mode' class on button
 
-        if ( localStorage.getItem( tibIinitator.storageKey + '-TIBBED' ))   setTibbed(e);   // set button to tibbed state
+        if ( localStorage.getItem( tibIinitator.storageKey + '-TIBBED' ))   setTibbed();   // set button to tibbed state
         
         if ( e.tagName === 'BUTTON' && !e.getAttribute('type'))  e.setAttribute( 'type', 'button' );   // prevent default submit type/action if within <form>
 
-        this.tibInitiator= tibInitiator;
-        e.tibInitiator= tibInitiator;
-        this.domElement= e;
-        e.tibButton= this;
+
+
         
-    };
+    // }; enclosing sub-functions, so in theory this and e and tibInitiator are available?
 
 
 
-    var setCounterElement= function( e) {
+        var setCounterElement= function( ) {
 
-        // called on button construct and after style button imported
+            // called on button construct and after style button imported
 
-        e.tibButton.counterElement= e.getElementsByClassName( 'bd-btn-counter')[0] || null;
-        if ( this.counterElement )   tibInitiator.updateQty();   // get initiator to trigger event to update counter
-    };
-
-
-
-    var writeCounter= function( e, QTY) {
-        if ( e.tibButton.counterElement && !isNaN(QTY) && QTY !== '' && QTY !== null) {    // isNaN('') will return false
-            e.tibButton.counterElement.textContent= parseInt(QTY, 10);
-        }
-    };
+            this.counterElement= e.getElementsByClassName( 'bd-btn-counter')[0] || null;
+            if ( this.counterElement )   tibInitiator.updateQty();   // get initiator to trigger event to update counter
+        };
 
 
 
-    var setTibbed= function(e) {
-
-        // set the button to tibbed state
-
-        e.tibButton.tibbed= true;
-        e.classList.add('tibbed');
-    };
+        var writeCounter= function( QTY) {
+            if ( this.counterElement && !isNaN(QTY) && QTY !== '' && QTY !== null) {    // isNaN('') will return false
+                this.counterElement.textContent= parseInt(QTY, 10);
+            }
+        };
 
 
 
-    var setTestnet= function(e) {
+        var setTibbed= function() {
 
-        // set the button to tibbed state
+            // set the button to tibbed state  
+            // perhaps can't use 'e' as also called from event handler
 
-        e.tibButton.testnet= true;
-        e.classList.add('testnet');
-    };
+            this.tibbed= true;
+            e.classList.add('tibbed');
+        };
 
 
 
-    var storageUpdate= function(ev) {
+        var setTestnet= function() {
 
-        // localStorage/tibstate listener to update the buttons counter
-        // used as the callback for tibHandler.tibInitiator, and when a Tib is acknowledged
-        // for tibstate custom event, detail attribute contains the localStorage key
-        // bound to DOM element, so this == e
+            // set the button to tibbed state
 
-        if(ev.type === 'tibstate') {
-            ev.key= ev.detail;
-            ev.newValue= localStorage[ev.key];
-        }
+            this.testnet= true;
+            e.classList.add('testnet');
+        };
 
-        if ( ev.newValue && ev.key === this.tibInitiator.storageKey + "-QTY" ) {
-            writeCounter( this, JSON.parse(ev.newValue).QTY);
+
+
+        var storageUpdate= function(ev, e) {
+
+            // localStorage/tibstate listener to update the buttons counter
+            // used as the callback for tibHandler.tibInitiator, and when a Tib is acknowledged
+            // for tibstate custom event, detail attribute contains the localStorage key
+            // bound to DOM element, so this == e  ////  CHANGED this now == tibButton
+
+            if(ev.type === 'tibstate') {
+                ev.key= ev.detail;
+                ev.newValue= localStorage[ev.key];
             }
 
-        if ( ev.newValue && ev.key === this.tibInitiator.storageKey + "-TIBBED" ) {
-            setTibbed( this);
-        }
-    };
+            if ( ev.newValue && ev.key === tibInitiator.storageKey + "-QTY" ) {
+                writeCounter( JSON.parse(ev.newValue).QTY);
+                }
 
+            if ( ev.newValue && ev.key === tibInitiator.storageKey + "-TIBBED" ) {
+                setTibbed( e);
+            }
+        };
+
+    }; // relocated from above to perhaps get access to e, this, tibInitiator...
 
 
 
@@ -140,8 +148,8 @@ var TIBIT= (function(tibit){
 
     // Expose public buttons methods/variables
     tibit.TibButton= TibButton;
-    buttons.initButtons= initButtons;
-    buttons.params= params;
+    tibit.initTibButtons= initButtons;
+    tibit.params= params;
 
     tibit.buttons= buttons;
 
