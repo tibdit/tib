1ccbad235326a75672b9054285ad0777b99d06ef
Nadil <nadil.bourkadi@tibdit.com>
2016-09-02 19:36:03 +0100

    tweak grunt to only use specified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt){
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             build: {
-                src: 'src/js/*.js',
+                src: ['src/js/tib-base.module.js', 'src/js/tib-button.module.js' , 'src/js/tib-callback.module.js', 'src/js/tib-element.module.js', 'src/js/tib-initiator.module.js'],
                 dest: 'dist/tib.min.js'
             }
         }
@@ -11,6 +11,8 @@ module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify'], function(){
+        grunt.log.write('Compiled tib.min.js ').ok();
+    });
 
 };
